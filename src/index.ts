d6b56b23ce6e52db9bd2cd9e607d88c81348d15a
brian <kipkoechbrian0@gmail.com>
2025-02-12 11:46:34 +0300

    typescript enums

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 console.log("hello world");
 let age: number = 20;
 console.log(age);
+
+enum Size {
+  Small = "s",
+  Medium = "m",
+  Large = "l",
+}
+const mySize: Size = Size.Large;
+console.log(mySize);
