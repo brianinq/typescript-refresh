628ad639d1a424c0bce651cbd7015cf626639842
brian <kipkoechbrian0@gmail.com>
2025-02-16 11:10:41 +0300

    parameterized decorators and decorator factory

--- dist/decorators.js
+++ dist/decorators.js
@@ -5,15 +5,35 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Component(constructor) {
-    console.log("component decorator called");
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertIntoDOM = () => {
-        console.log("Inserting the component into the dom");
+function Component(name) {
+    return (constructor) => {
+        console.log(`component decorator called from ${name}`);
+        constructor.prototype.uniqueId = Date.now();
+        constructor.prototype.insertIntoDOM = () => {
+            console.log("Inserting the component into the dom");
+        };
     };
 }
 let ProfileComponent = class ProfileComponent {
 };
 ProfileComponent = __decorate([
-    Component
+    Component("profile Component")
 ], ProfileComponent);
+function Log(target, methodName, descriptor) {
+    const originalFunction = descriptor.value;
+    descriptor.value = function (...args) {
+        console.log("Before method call");
+        originalFunction.call(this, ...args);
+        console.log("after method call");
+    };
+}
+class Person {
+    say(message) {
+        console.log("the message is" + message);
+    }
+}
+__decorate([
+    Log
+], Person.prototype, "say", null);
+const p1 = new Person();
+p1.say("hello");
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,10 +1,39 @@
-function Component(constructor: Function) {
-  console.log("component decorator called");
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertIntoDOM = () => {
-    console.log("Inserting the component into the dom");
+// function Component(constructor: Function) {
+//   console.log("component decorator called");
+//   constructor.prototype.uniqueId = Date.now();
+//   constructor.prototype.insertIntoDOM = () => {
+//     console.log("Inserting the component into the dom");
+//   };
+// }
+
+//decorator factory
+function Component(name: string) {
+  return (constructor: Function) => {
+    console.log(`component decorator called from ${name}`);
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertIntoDOM = () => {
+      console.log("Inserting the component into the dom");
+    };
   };
 }
 
-@Component
+@Component("profile Component")
 class ProfileComponent {}
+
+// method decorators
+function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const originalFunction = descriptor.value as Function;
+  descriptor.value = function (...args: any) {
+    console.log("Before method call");
+    originalFunction.call(this, ...args);
+    console.log("after method call");
+  };
+}
+class Person {
+  @Log
+  say(message: string) {
+    console.log("the message is" + message);
+  }
+}
+const p1 = new Person();
+p1.say("hello");
